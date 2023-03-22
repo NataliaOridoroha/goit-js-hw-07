122f7fe9de6fec3d34bd8225e50e21e58711916b
NataliaOridoroha <nk121418@gmail.com>
2023-03-22 18:24:31 +0200

    goit-js-hw-07

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+function createLi(array) {
+    return array.reduce(
+        (acc, item) =>
+        acc + 'li gallery__item = "${item.description}">${item.original}>${item.preview}</li>', ""
+    );
+}
+
+console.log(createLi(galleryItems));
+
+
+
+
+// <li class="gallery__item">
+//   <a class="gallery__link" href="large-image.jpg">
+//     <img
+//       class="gallery__image"
+//       src="small-image.jpg"
+//       data-source="large-image.jpg"
+//       alt="Image description"
+//     />
+//   </a>
+// </li>
+
+
+
+
+
+
+
+
+
+
+
+
+
 console.log(galleryItems);
